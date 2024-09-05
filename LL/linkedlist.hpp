9bc13c58c702904dea77c8f00766cc34143c4b83
John Marangola <jmarangola@softroboticsinc.com>
2024-09-05 15:44:00 -0400

    add header for linkedlist.

---
+++ LL/linkedlist.hpp
@@ -0,0 +1,15 @@
+#ifndef LINKEDLIST_HPP_
+#define LINKEDLIST_HPP_
+
+// Your header goes here
+template <typename T>
+class LinkedList
+{
+  // TODO:
+public:
+  // TODO:
+private:
+  // TODO:
+};
+
+#endif
