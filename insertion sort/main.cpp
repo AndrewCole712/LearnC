368a2e40ea81c77ec748b9cb023050574b2582f4
Andrew Cole <andrewcole.asc@gmail.com>
2024-09-05 11:12:56 -0400

    insertion sort first attempt. having issues with passing an array to the functions I wrote

---
+++ insertion sort/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdlib>
+
+void insertion_sort(int nums, int size)
+{
+    for (int i {1}; i < size; i++)
+    {
+        int key = nums[i];
+        int j = i-1;
+
+        while (j > 0 and nums[j] > key)
+        {
+            nums[j+1] = nums[j];
+            j--;
+        }
+    }
+}
+/* for each element in the list (nums[i] or "key"), we go through 
+        and as long as it's less than the number immediately 
+        to its left ("nums[j] > key"), we swap the position of the 
+        elements ("nums[j+1] = nums [j]"). 
+        Starting with the element second to the left ("int i=1"), 
+        we ensure that the entire list is sorted */
+
+void print_array( int nums, int size)
+{
+    std::cout << nums << "\n";
+}
+
+int array_builder(int size)
+{
+    int array[ size ];
+    for (int i = 0; i < size; i++)
+    {
+        rand() % 100;
+    };
+
+}
+
+int main()
+{
+    std::cout << "Enter the size of array to be sorted: ";
+    int x{};
+    std::cin>>x;
+
+    int array_to_sort = array_builder(x);
+    insertion_sort(array_to_sort, x);
+    print_array(array_to_sort, x);
+
+    return 0;
+}
